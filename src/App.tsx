e9a6e2de875076b979663c375a4e435ec4ecd296
ponte1010 <t.miwa.528@nitech.jp>
2022-09-24 13:24:29 +0900

    Add RadarPlot

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import './App.css';
 import LinePlot from './components/LinePlot';
 import PiePlot from './components/PiePlot';
 import BarPlot from './components/BarPlot';
+import RadarPlot from './components/RadarPlot';
 
 function App() {
   return <div className="App">
     <LinePlot/>
     <PiePlot/>
     <BarPlot/>
+    <RadarPlot/>
   </div>;
 }
 
