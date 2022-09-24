940135b5e36afbd90c9632e8855dab1881057392
ponte1010 <t.miwa.528@nitech.jp>
2022-09-24 13:00:20 +0900

    Add BarPlot

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import LinePlot from './components/LinePlot';
 import PiePlot from './components/PiePlot';
+import BarPlot from './components/BarPlot';
 
 function App() {
   return <div className="App">
     <LinePlot/>
     <PiePlot/>
+    <BarPlot/>
   </div>;
 }
 
